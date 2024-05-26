282306686348b95e6a28229c278d04461181b14a
juansadeveloper <juansapereira@hotmail.com>
2024-05-26 11:10:12 +0200

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -64,7 +64,7 @@ function Navbar() {
                   <Link href="#about"><li className='hover:text-white/85 transition duration-300  rounded px-4 py-1 cursor-pointer'>ABOUT</li></Link>
                   <Link href="#contact"><li className='hover:text-white/85 transition duration-300  rounded px-4 py-1 cursor-pointer'>CONTACT</li></Link>
                   <li className='hover:text-white/85 transition duration-300  rounded px-4 py-2 cursor-pointer'>UPCOMING RETREATS</li>
-                  <div className='w-full h-[3px] bg-white my-10'></div>
+                  <div className='w-full h-[3px] bg-white my-7'></div>
                   <div>
                     <p className='text-justify  text-accent font-medium text-xl mb-5'>Connect with Hannah</p>
                     <div  className='flex justify-center items-center gap-5 '>
@@ -73,7 +73,7 @@ function Navbar() {
                       <Link href="https://www.instagram.com/eagnawithhannah/"><AiOutlineInstagram size={30} className='text-accent hover:scale-[1.05]  cursor-pointer transition duration-300'/></Link>
                     </div> 
                   </div>
-                  <div className='w-full h-[3px] bg-white my-10'></div>
+                  <div className='w-full h-[3px] bg-white my-7'></div>
                   <Link href="/"><div className='mt-10 text-[#623810]/85 hover:scale-[1.05] transition duration-300 font-light  text-[24px]'><span className='font-semibold text-[24px]'>EAGNA</span> WITH HANNAH</div></Link>
                   {/* <p className='mt-20'>#TRANSFORM YOUR LIFE WITH YOGA.</p> */}
                   <p className='mt-3 font-light'>#YOGA IS OUR BEST CHOICE.</p>
