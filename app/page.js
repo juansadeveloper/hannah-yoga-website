b0669a22957e6f11c9a21665fd1485b80af23b70
juansadeveloper <juansapereira@hotmail.com>
2024-05-26 14:48:22 +0200

    Big change

--- app/page.js
+++ app/page.js
@@ -3,20 +3,15 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Join from "./components/Join";
-import Why from "./components/Why";
-import Quote from "./components/Quote";
+
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
-      <About /> 
-      <Join />
-      <Why />
-      <Quote />
-      <Contact /> 
+      {/* <About /> */}
+      {/* <Contact /> */}
       <Footer />  
     </main>
   );
