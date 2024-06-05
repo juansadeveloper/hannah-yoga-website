c2d77666976021d7655f09ef53b44ed0caa29a8a
juansadeveloper <juansapereira@hotmail.com>
2024-06-05 17:28:22 +0200

    Update page.jsx

--- app/fundraising/page.jsx
+++ app/fundraising/page.jsx
@@ -8,7 +8,7 @@ function page() {
 
         <div className='w-full h-[80px] bg-[#EADDD4] z-20 fixed top-0 flex items-center justify-center px-5'>
             <div className='flex items-center justify-between  w-full max-w-[1500px]'>
-                <Link href="/"><div className='text-[#623810]/85 font-light text-[18px]  md:text-[24px] hover:scale-[1.05] transition duration-300'><span className='font-semibold text-[18px] md:text-[24px]'>FUNDRAISING</span></div></Link>
+                <Link href="/"><div className='text-[#623810]/85 font-light text-[18px]  md:text-[24px] hover:scale-[1.05] transition duration-300'><span className='font-semibold text-[18px] md:text-[24px]'>EAGNA</span> WITH HANNAH</div></Link>
                 <div className='flex max-md:flex-col md:gap-5 items-center'>
                     {/* <h1 className='text-[15px] md:text-[24px] font-bold text-[#623810]/85'> YOGA</h1> */}
                     <Link href="/#contact"><button className='px-2 py-1 md:px-3 md:py-2 bg-white rounded text-[#623810]/85 border border-[#623810]/20 hover:border-[#623810]/50 font-medium transition duration-300 max-md:tracking-tight'>CONTACT</button></Link>
