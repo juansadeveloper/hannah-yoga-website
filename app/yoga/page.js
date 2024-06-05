efb0d4fed12f60394eb6abbfcf46aa08f39c932b
juansadeveloper <juansapereira@hotmail.com>
2024-06-05 15:33:22 +0200

    Updated Style Retreat Page

--- app/yoga/page.js
+++ app/yoga/page.js
@@ -34,7 +34,7 @@ function Page() {
 
         <div className='w-full h-[80px] bg-[#EADDD4] z-20 fixed top-0 flex items-center justify-center px-5'>
             <div className='flex items-center justify-between  w-full max-w-[1500px]'>
-                <Link href="/"><div className='text-[#623810]/85 font-light  md:text-[24px] hover:scale-[1.05] transition duration-300'><span className='font-semibold md:text-[24px]'>YOGA</span> WITH HANNAH</div></Link>
+                <Link href="/"><div className='text-[#623810]/85 font-light  md:text-[24px] hover:scale-[1.05] transition duration-300 text-[18px]'><span className='font-semibold text-[18px] md:text-[24px]'>YOGA</span> WITH HANNAH</div></Link>
                 <div className='flex max-md:flex-col md:gap-5 items-center'>
                     {/* <h1 className='text-[15px] md:text-[24px] font-bold text-[#623810]/85'> YOGA</h1> */}
                     <button className='px-2 py-1 md:px-3 md:py-2 bg-white rounded text-[#623810]/85 border border-[#623810]/20 hover:border-[#623810]/50 font-medium transition duration-300 max-md:tracking-tight'>Book a session</button>
